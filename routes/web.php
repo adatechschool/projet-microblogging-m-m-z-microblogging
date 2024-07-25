37d3b4e188cd35921628a68e08cc78886d7c7eb5
Z0uLf4T <A.Zoulfat@gmail.com>
2024-07-25 12:24:23 +0000

    install de breeze et gestion des authentifications

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Quand l'utilisateur se connecte affiche le dashboard (middleware 'auth' vérifie : email et mdp;
+ lien vers logout et vers profile),
+ si aucun utilisateur est connecté renvoie à la page login (middleware 'guest')*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+/* Quand l'utilisateur est connecté affiche la page profile (middleware 'auth' form pour edit l'user, pour changer de mdp et pour supprimer l'user),
+ si aucun utilisateur est connecté renvoie à la page login (middleware 'guest') */
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
