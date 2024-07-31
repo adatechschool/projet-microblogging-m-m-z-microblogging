a12a9cd3da6e6e53d0c3576dc84045dbb92fc63d
Marine Suhard <marine.suhard@gmail.com>
2024-07-31 12:57:27 +0200

    update database table posts

--- database/migrations/2024_07_26_151025_create_posts_table.php
+++ database/migrations/2024_07_26_151025_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
-            $table->string('title');
+            $table->string('title')->nullable();
             $table->string('picture');
             $table->text('content');
             $table->timestamps();
