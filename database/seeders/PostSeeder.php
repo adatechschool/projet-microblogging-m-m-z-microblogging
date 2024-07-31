d926b080ac12db94450901f86ec582763ad60b80
Marine Suhard <marine.suhard@gmail.com>
2024-07-31 15:20:19 +0200

    manage conflict PostSeeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -6,13 +6,16 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Post;
 
+
 class PostSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
+
     public function run()
     {
         Post::factory()->count(20)->create();
+
     }
 }
