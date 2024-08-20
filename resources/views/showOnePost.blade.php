f4c65f14a63df37f99ed8db36b73a86a1abbde59
ManonSella <manonsella@gmail.com>
2024-08-20 15:39:54 +0200

    adds alt text for img

--- resources/views/showOnePost.blade.php
+++ resources/views/showOnePost.blade.php
@@ -1,2 +1,2 @@
 <h1>{{ $post->content }}</h1>
-<!--<img src="{{ Storage::url($post->picture) }}" alt="" />
+<!--<img src="{{ Storage::url($post->picture) }}" alt="user's selected and uploaded picture to be shown on the same user's wall." />
